5d2cb2a55729f5bda3fb388ca9c6473770095998
Fadhli Dzil Ikram <adzil13@gmail.com>
2016-11-18 17:44:30 +0700

    Rewrite Setlist.js from stratch v0.2.0
    Handles local try-catch with yield, proxy generator function classes or objects with Promise, and callbackify generator function as described in README.md. Remove non-working and silly timeout function implementation.

--- test/setlist.js
+++ test/setlist.js
@@ -4,156 +4,247 @@
  * @author Fadhli Dzil Ikram
  */
 
-'use strict';
-
 const $ = require('../lib/setlist');
-const s = require('./snippets');
 const should = require('should');
 
-describe('Method probes', function() {
-    it('$ should be a function and take 2 arguments', function() {
-        $.should.be.a.Function().with.lengthOf(2);
-    });
-    it('$.identify() should be a function and take 1 argument', function() {
-        $.identify.should.be.a.Function().with.lengthOf(1);
-    });
-    it('$.next() should be a function and take 1 argument', function() {
-        $.next.should.be.a.Function().with.lengthOf(1);
-    });
-    it('$.async() should be a function and take 1 argument', function() {
-        $.async.should.be.a.Function().with.lengthOf(1);
-    });
-    it('$.convert() should be a function and take 1 argument', function() {
-        $.convert.should.be.a.Function().with.lengthOf(1);
-    });
-    it('$.worker() should be a function and take 4 arguments', function() {
-        $.worker.should.be.a.Function().with.lengthOf(4);
-    });
-});
-describe('Object identifier $.identify() test', function() {
-    it('Should recognize promise object', function() {
-        $.identify(s.prom).should.equal('Promise');
-    });
-    it('Should recognize generator function', function() {
-        $.identify(s.gf()).should.equal('GeneratorFunction');
-    });
-    it('Should recognize plain function', function() {
-        $.identify(s.fn).should.equal('Function');
-    });
-    it('Should not recognize plain object', function() {
-        $.identify(s.obj).should.equal(false);
-    });
-});
-describe('Async wrapper $.async() test', function() {
-    it('Should throw when passed non-function object', function() {
-        (function() {
-            $.async(s.obj);
-        }).should.throwError('InvalidParameter');
-    });
-    it('Should return as function', function() {
-        $.async(s.async).should.be.a.Function();
-    });
-    it('Should return promise when return Fn invoked', function() {
-        $.identify($.async(s.async)()).should.equal('Promise');
-    });
-    it('Should resolve the promise with no parameter', function() {
-        return $.async(s.callback)(undefined)
-            .should.fulfilledWith(undefined);
-    });
-    it('Should resolve the promise with single parameter', function() {
-        return $.async(s.callback)(true)
-            .should.fulfilledWith(true);
-    });
-    it('Should resolve the promise with multiple parameter', function() {
-        return $.async(s.callbackMulti)(true, false, null, 1)
-            .should.fulfilledWith([true, false, null, 1]);
-    });
-});
-describe('Task worker $.worker() test', function() {
-    it('Should return value from GenFn', function() {
-        return s.probe($.worker, s.gf(true)).should
-            .fulfilledWith(true);
-    });
-    it('Should return value from child GenFn', function() {
-        return s.probe($.worker, s.gfParent(true)).should
-            .fulfilledWith(true);
-    });
-    it('Should return value from promise', function() {
-        return s.probe($.worker, s.gfPromise(true)).should
-            .fulfilledWith(true);
-    });
-    it('Should return value from callback Fn', function() {
-        return s.probe($.worker, s.gfCallback(true)).should
-            .fulfilledWith(true);
-    });
-    it('Should return value from yielded value', function() {
-        return s.probe($.worker, s.gfYield(true)).should
-            .fulfilledWith(true);
-    });
-    it('Should return value from mixed yields', function() {
-        return s.probe($.worker, s.gfMix(true)).should
-            .fulfilledWith(true);
-    });
-    it('Should return value from parent mixed yields', function() {
-        return s.probe($.worker, s.gfMixParent(true)).should
-            .fulfilledWith(true);
-    });
-    it('Should handle synchronous throw', function() {
-        return s.probe($.worker, s.gfErrSync()).should
-            .rejectedWith('SnippetError');
-    });
-    it('Should handle child synchronous throw', function() {
-        return s.probe($.worker, s.gfErrParent()).should
-            .rejectedWith('SnippetError');
-    });
-    it('Should handle promise throw', function() {
-        return s.probe($.worker, s.gfErrPromise()).should
-            .rejectedWith('SnippetError');
-    });
-});
-describe('Setlist $ test', function() {
-    it('Should throw error on non-GenFn parameter input', function() {
-        (function() {
-            $(s.fn());
-        }).should.throwError('InvalidParameter');
-    });
-    it('Should give timeout error when timeout elapsed', function() {
-        return $(s.gfLong(), 1).should.rejectedWith('TimeoutError');
-    });
-    it('Should retun promise', function() {
-        $.identify($(s.gf())).should.equal('Promise');
-    });
-    it('Should chainable with .next() function', function() {
-        $(s.gf()).next.should.be.a.Function();
-    });
-    it('Chainable .next() should also return promise', function() {
-        $.identify($(s.gf()).next(s.gf())).should.equal('Promise');
-    });
-    it('Should correctly return value after .next()', function() {
-        return $(s.gf()).next(s.gf(true)).should.fulfilledWith(true);
-    });
-});
-describe('Setlist $.convert() test', function() {
-    let target = undefined;
+let promise = Promise.resolve();
+let genFn = function* () {};
+let fn = function () {};
+let obj = {};
 
-    before(function() {
-        // Start convertion process
-        target = $.convert(s.objSrc);
-    });
-
-    it('Target should only have 1 own property', function() {
-        Object.getOwnPropertyNames(target).should.have.lengthOf(1);
-    });
-    it('Target should have same version of fn()', function() {
-        target.fn.should.equal(s.objSrc.fn);
-    });
-    it('Target gf() should be a function', function() {
-        target.gf.should.be.a.Function();
-    });
-    it('Target gf() should return a promise', function() {
-        $.identify(target.gf()).should.equal('Promise');
-    });
-    it('Target gf() should eventually return value', function() {
-        return target.gf(true).should.eventually.equal(true);
-    });
+describe('Internal type checking test', function() {
+  it('isPromise() should correctly identify promise-like', function() {
+    $.isPromise(promise).should.equal(true);
+    $.isPromise(genFn).should.equal(false);
+    $.isPromise(genFn()).should.equal(false);
+    $.isPromise(fn).should.equal(false);
+    $.isPromise(obj).should.equal(false);
+    $.isPromise(undefined).should.equal(false);
+  });
+  it('isGenerator() should correctly identify generator', function() {
+    $.isGenerator(promise).should.equal(false);
+    $.isGenerator(genFn).should.equal(false);
+    $.isGenerator(genFn()).should.equal(true);
+    $.isGenerator(fn).should.equal(false);
+    $.isGenerator(obj).should.equal(false);
+    $.isPromise(undefined).should.equal(false);
+  });
+  it('isGeneratorFunction() should correctly identify gen fn', function() {
+    $.isGeneratorFunction(promise).should.equal(false);
+    $.isGeneratorFunction(genFn).should.equal(true);
+    $.isGeneratorFunction(genFn()).should.equal(false);
+    $.isGeneratorFunction(fn).should.equal(false);
+    $.isGeneratorFunction(obj).should.equal(false);
+    $.isGeneratorFunction(undefined).should.equal(false);
+  });
 });
+
+describe('Generator runner sanity checking test', function() {
+  it('Should return identical promise on promise input', function() {
+    $(promise).should.equal(promise);
+  });
+  it('Should return promise on generator function input', function() {
+    $(genFn).should.be.a.Promise();
+  });
+  it('Should return promise on generator input', function() {
+    $(genFn()).should.be.a.Promise();
+  });
+  it('Should throw on invalid input (ordinary function)', function() {
+    (function() {
+      $(fn)
+    }).should.throw();
+  });
+  it('Should throw on invalid input (ordinary object)', function() {
+    (function() {
+      $(obj)
+    }).should.throw();
+  });
+});
+
+describe('Generator runner functional test', function() {
+  it('Non-async return', function() {
+    let gf = function* (v) { return v };
+    return $(gf(true)).should.fulfilledWith(true);
+  });
+  it('Non-async yield return', function() {
+    let gf = function* (v) { return yield v };
+    return $(gf(true)).should.fulfilledWith(true);
+  });
+  it('Promise return', function() {
+    let gf = function* (v) { return Promise.resolve(v) };
+    return $(gf(true)).should.fulfilledWith(true);
+  });
+  it('Promise yield return', function() {
+    let gf = function* (v) { return yield Promise.resolve(v) };
+    return $(gf(true)).should.fulfilledWith(true);
+  });
+  it('Generator function return', function() {
+    let gf = function* (v) { return function* (v) { return v }(v) };
+    return $(gf(true)).should.fulfilledWith(true);
+  });
+  it('Generator function yield return', function() {
+    let gf = function* (v) { return yield function* (v) { return v }(v) };
+    return $(gf(true)).should.fulfilledWith(true);
+  });
+});
+
+describe('Generator runner error catching test', function() {
+  it('Catch promise error globally', function() {
+    let gf = function* () { yield Promise.reject(new Error('TestError')) };
+    return $(gf).should.be.rejectedWith('TestError');
+  });
+  it('Catch promise error locally', function() {
+    let gf = function* () {
+      try {
+        yield Promise.reject(new Error('TestError'));
+      } catch(err) {
+        if (err.message === 'TestError') {
+          return true;
+        }
+        throw err;
+      }
+    };
+    return $(gf).should.fulfilledWith(true);
+  });
+  it('Catch generator function error globally', function() {
+    let gf = function* () {
+      yield function* () {
+        throw new Error('TestError');
+      }();
+    };
+    return $(gf).should.be.rejectedWith('TestError');
+  });
+  it('Catch generator function error locally', function() {
+    let gf = function* () {
+      try {
+        yield function* () {
+          throw new Error('TestError');
+        }();
+      } catch(err) {
+        if (err.message === 'TestError') {
+          return true;
+        }
+        throw err;
+      }
+    };
+    return $(gf).should.fulfilledWith(true);
+  });
+});
+
+describe('Utility sanity checking test', function() {
+  it('Callbackify should return function', function() {
+    $.callbackify(genFn).should.be.a.Function();
+  });
+  it('Callbackify should throw on non-gen fn input', function() {
+    (function() {
+      $.callbackify(fn);
+    }).should.throw();
+  });
+  it('Promisify should return function', function() {
+    $.promisify(fn).should.be.a.Function();
+  });
+  it('Promisified function should return promise on call', function() {
+    $.promisify(fn)().should.be.a.Promise();
+  });
+  it('Promisify should throw on non-function input', function() {
+    (function() {
+      $.promisify(obj);
+    }).should.throw();
+  });
+  it('Proxify should throw on non-object input', function() {
+    (function() {
+      $.proxify('obj');
+    }).should.throw();
+  });
+});
+
+describe('Callbackify functional test', function() {
+  let gfCallback = function* (v) { return v }
+  let gfCallbackError = function* () { throw new Error('TestError') }
+  let callbackParent = function (fn, v) {
+    return new Promise(function(resolve, reject) {
+      $.callbackify(fn)(v, callbackHandle);
+
+      function callbackHandle(err, value) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(value);
+        }
+      }
+    });
+  }
+
+  it('Should resolve as value when return from gen fn', function() {
+    return callbackParent(gfCallback, true).should.fulfilledWith(true);
+  });
+  it('Should catch error from gen fn and map proper callback', function() {
+    return callbackParent(gfCallbackError, true).should
+        .rejectedWith('TestError');
+  });
+});
+
+describe('Promisify functional test', function() {
+  let callbackFn = function(err, v, cb) {
+    process.nextTick(() => cb(err, v));
+  }
+  let callbackMulti = function(err, v, w, x, cb) {
+    process.nextTick(() => cb(err, v, w, x));
+  }
+  
+  it('Should properly pass value to the promise .then()', function() {
+    return $.promisify(callbackFn)(null, true).should.fulfilledWith(true);
+  });
+  it('Should properly pass error to .catch()', function() {
+    return $.promisify(callbackFn)(new Error('TestError'), null).should
+        .rejectedWith('TestError');
+  });
+  it('Should properly pass array to .then() on multi arguments', function() {
+    return $.promisify(callbackMulti)(null, true, 1, false).should
+        .fulfilledWith([true, 1, false]);
+  });
+});
+
+describe('Proxify functional test', function() {
+  let baseClass = class {
+    * base() {
+      return yield Promise.resolve(this.var);
+    }
+
+    setVar(v) {
+      this.var = v;
+    }
+
+    static * base() {
+      return yield Promise.resolve(this.var);
+    }
+
+    static setVar(v) {
+      this.var = v;
+    }
+  }
+  let extendedClass = class extends baseClass {
+    * ext() {
+      return yield this.base();
+    }
+
+    static * ext() {
+      return yield this.base();
+    }
+  }
+
+  // Proxify class
+  $.proxify(baseClass);
+  $.proxify(extendedClass);
+
+  extendedClass.setVar(true);
+  let extendedObject = new extendedClass();
+  extendedObject.setVar(true);
+
+  it('Should properly resolve this keyword on prototype', function() {
+    return extendedObject.ext().should.fulfilledWith(true);
+  });
+  it('Should properly resolve this keyword on constructor', function() {
+    return extendedClass.ext().should.fulfilledWith(true);
+  });
+})
